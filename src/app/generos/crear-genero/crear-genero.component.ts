14e8d0e65f05e14012d0addff15da867e0dc7bb9
Luigi Alberto Antoine Terrero <lantoine@dgii.gov.do>
2022-03-23 23:30:34 -0400

    Menu, ruteos, estilo en el front

---
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-crear-genero',
+  templateUrl: './crear-genero.component.html',
+  styleUrls: ['./crear-genero.component.css']
+})
+export class CrearGeneroComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  guardarCambios(){
+    //Esto se va a comunicarse con el web api
+    this.router.navigate(['/generos'])
+  }
+}
